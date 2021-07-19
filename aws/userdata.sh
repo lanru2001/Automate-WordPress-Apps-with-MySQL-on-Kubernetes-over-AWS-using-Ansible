e1f295ba368e1fdc7c60dc9fde39b63853f75de5
Azeez Temitope Olanrewaju <59709429+lanru2001@users.noreply.github.com>
2021-07-18 22:19:16 -0500

    Update userdata.sh

--- aws/userdata.sh
+++ aws/userdata.sh
@@ -13,7 +13,6 @@ sudo yum -y install wget
 sudo yum -y install git
 
 #Install aws cli 
-
 # Install the latest system updates.
 sudo yum -y update  
 # Install the AWS CLI
@@ -21,6 +20,44 @@ sudo yum -y install aws-cli
 # Confirm the AWS CLI was installed.
  aws --version              
 
+#Install Docker and Docker Compose
+# Remove any old versions
+sudo yum remove docker docker-common docker-selinux docker-engine
+
+# Install required packages
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+
+# Configure docker repository
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+
+# Install Docker-ce
+sudo yum install docker-ce -y
+
+# Start Docker
+sudo systemctl start docker
+sudo systemctl enable docker
+
+# Post Installation Steps
+# Create Docker group
+sudo groupadd docker
+
+# Add user to the docker group
+sudo usermod -aG docker $USER
+
+echo "Installation Complete -- Logout and Log back"
+
+# Install docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.25.3/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+# Permssion +x execute binary
+chmod +x /usr/local/bin/docker-compose
+
+# Create link symbolic 
+ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
+# Check Version docker-compose
+echo "Installation Complete -- Logout and Log back"
+docker-compose --version
 
 #Java installation
 wget --no-cookies --header "Cookie: gpw_e24=xxx; oraclelicense=accept-securebackup-cookie;" "http://download.oracle.com/otn-pub/java/jdk/8u11-b12/jdk-8u11-linux-x64.rpm"
